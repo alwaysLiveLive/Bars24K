b4193835f50e8a51e5ad3556020cb3e97a58d2f7
alwaysLiveLive <86807911+alwaysLiveLive@users.noreply.github.com>
2022-03-01 20:32:32 -0800

    Update randsongs.py

--- randsongs.py
+++ randsongs.py
@@ -15,14 +15,18 @@ def getRandSong():
 def songCrazyMashup(song, num):
   origLength = len(song)
   for i in range(num):
-    randStart1 = random.randrange(0, int(len(song)/3))
-    randEnd1 = random.randrange(randStart1, int(len(song)/3))
-    randStart2 = random.randrange(int(len(song)/3), int(len(song)*2/3))
-    randEnd2 = random.randrange(randStart2, int(len(song)*2/3))
-    randStart3 = random.randrange(int(len(song)*2/3), len(song))
+    randStart1 = random.randrange(0, len(song)//3)
+    randEnd1 = random.randrange(randStart1, len(song)//3)
+    randStart2 = random.randrange(len(song)//3, 2*len(song)//3)
+    randEnd2 = random.randrange(randStart2, 2*len(song)//3)
+    randStart3 = random.randrange(2*len(song)//3, len(song))
     randEnd3 = random.randrange(randStart3, len(song))
     song = song[:randStart1] + song[randEnd1:] + song[randStart1:randEnd1]
     song = song[randStart2:randEnd2] + song[:randStart2] + song[randEnd2:]
     lastRand = random.randrange(0, len(song))
     song = song[:lastRand] + song[randStart3:randEnd3] + song[lastRand:]
   return song[:origLength]
+
+def getNameSong(name):
+  name = name.upper()
+  return name * (2000 // len(name))
